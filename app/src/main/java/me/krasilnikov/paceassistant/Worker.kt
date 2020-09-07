a79edea966c5bb653b1eb7eb60dc423efba109ec
Alexey Krasilnikov <alexey@krasilnikov.me>
2020-09-07 23:50:45 +0300

    Increase step between same announcements to 30 seconds

--- app/src/main/java/me/krasilnikov/paceassistant/Worker.kt
+++ app/src/main/java/me/krasilnikov/paceassistant/Worker.kt
@@ -321,7 +321,7 @@ object Worker {
 
             val update = when {
                 lastTime == 0L -> true
-                now - lastTime > TimeUnit.SECONDS.toMillis(10) -> true
+                now - lastTime > TimeUnit.SECONDS.toMillis(30) -> true
                 abs(lastHR - hr) > 10 -> true
                 else -> false
             }
