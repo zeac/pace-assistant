9d9cd5d9378e209e8a0a97f6c489f99a51d8bb61
Alexey Krasilnikov <alexey@krasilnikov.me>
2021-02-01 10:23:03 +0300

    Jetpack Compose 1.0.0-alpha11

--- app/src/main/java/me/krasilnikov/paceassistant/MainActivity.kt
+++ app/src/main/java/me/krasilnikov/paceassistant/MainActivity.kt
@@ -45,7 +45,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.onDispose
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
@@ -55,8 +54,6 @@ import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
 
@@ -70,13 +67,7 @@ class MainActivity : AppCompatActivity() {
 
         setContent {
             PaceTheme {
-                val job = Job()
-                val coroutineContext = Dispatchers.Main.immediate + job
-                onDispose {
-                    job.cancel()
-                }
-
-                val state by Worker.state.collectAsState(context = coroutineContext)
+                val state by Worker.state.collectAsState()
 
                 contentForState(state)
             }
